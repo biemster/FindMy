a7185883d34e0edc35cc77ed28e7d421bc691d58
Lars Beemster <l.j.beemster@gmail.com>
2022-05-31 22:43:42 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FindMy
-Query Apple's Find My network
+Query Apple's Find My network, based on all the hard work of https://github.com/seemoo-lab/openhaystack/ and others.
+
+## Install
+Installation is just 2 easy steps (4 if you don't have a Mac and Apple ID)
+1. Download the latest release from https://github.com/n3d1117/cook, and follow the install instructions from there
+2. Add your public keys to `request_reports.sh` at the top, and run (only internal tools are used, no dependencies)
+
---
+++ request_reports.sh
@@ -0,0 +1,46 @@
+ids='["123","456"]'
+
+searchPartyToken=$(security find-generic-password -w -s 'com.apple.account.AppleAccount.search-party-token')
+AppleID_UUID=$(security find-generic-password -w -s 'iCloud')
+
+anisette=$(curl -s localhost:8080/anisette | python -c "import sys,json; print(json.load(sys.stdin)['base64_encoded_data'])" | base64 -D)
+machineID=$(python -c "import sys,json; print(json.loads('$anisette')['machineID'])")
+oneTimePassword=$(python -c "import sys,json; print(json.loads('$anisette')['oneTimePassword'])")
+routingInfo=$(python -c "import sys,json; print(json.loads('$anisette')['routingInfo'])")
+
+swver=$(sw_vers | sed -E 's/.*:[[:blank:]](.*)/\1/' | paste -d\; -s -)
+model=$(ioreg -d2 -c IOPlatformExpertDevice | awk -F\" '/model/{print $(NF-1)}')
+userAgent="searchpartyd/1 <$model>/<$swver>"
+
+clientTime=$(date +%Y-%m-%dT%H:%M:%S%z)
+timeZone=$(date +%Z)
+unixEpoch=$(date +%s)
+startDate=$(bc <<<"1654016611 - (86400*7)")
+
+auth_b64=$(echo "$searchPartyToken:$AppleID_UUID" | base64)
+
+data="{search: [{"endDate": $unixEpoch,"startDate": $startDate,"ids": $ids}]}"
+
+echo $clientTime $timeZone $unixEpoch
+echo "search-party-token:" $searchPartyToken
+echo "AppleID_UUID:" $AppleID_UUID
+echo "machineID:" $machineID
+echo "oneTimePassword:" $oneTimePassword
+echo "User-Agent:" $userAgent
+echo "data:" $data
+
+
+curl -v \
+ --header "Authorization: $auth_b64" \
+ --header "X-Apple_I_MD: $oneTimePassword" \
+ --header "X-Apple_I_MD-RINFO: $routingInfo" \
+ --header "X-Apple_I_MD-M: $machineID" \
+ --header "X-Apple_I_TimeZone: $timeZone" \
+ --header "X-Apple_I_ClientTime: $clientTime" \
+ --header "Content-Type: application/json" \
+ --header "Accept: application/json" \
+ --header "X-BA-CLIENT-TIMESTAMP: $unixEpoch" \
+ --user-agent "$userAgent" \
+ -d "$data" \
+ -X POST https://gateway.icloud.com/acsnservice/fetch
+
