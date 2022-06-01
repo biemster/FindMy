dd464387daca7cc1e26d878ef8299b4df0beba01
Lars Beemster <l.j.beemster@gmail.com>
2022-06-01 14:43:25 +0200

    fix Authorization header

--- request_reports.sh
+++ request_reports.sh
@@ -2,7 +2,7 @@
 
 ids='["123","456"]'
 
-AppleID_UUID=$(security find-generic-password -w -s 'iCloud')
+AppleID_UUID=$(security find-generic-password -s 'iCloud' | awk -F\" '/acct/{print $(NF-1)}')
 searchPartyToken=$(security find-generic-password -w -s 'com.apple.account.AppleAccount.search-party-token')
 
 anisette=$(curl -s localhost:8080/anisette | python -c "import sys,json; print(json.load(sys.stdin)['base64_encoded_data'])" | base64 -D)
@@ -14,11 +14,11 @@ swver=$(sw_vers | sed -E 's/.*:[[:blank:]](.*)/\1/' | paste -d\; -s -)
 model=$(ioreg -d2 -c IOPlatformExpertDevice | awk -F\" '/model/{print $(NF-1)}')
 userAgent="searchpartyd/1 <$model>/<$swver>"
 
-clientTime=$(date +%Y-%m-%dT%H:%M:%S%z)
+clientTime=$(date -u +%Y-%m-%dT%H:%M:%SZ)
 timeZone=$(date +%Z)
 unixEpoch=$(date +%s)
 endDate=$(($unixEpoch * 1000))
-startDate=$(bc <<<"$endDate - (86400000*7)")
+startDate=$(($endDate - (86400000*7)))
 
 auth_b64=$(echo "$AppleID_UUID:$searchPartyToken" | base64)
 
@@ -34,12 +34,12 @@ echo "data:" $data
 
 
 curl -v \
- --header "Authorization: $auth_b64" \
+ --header "Authorization: Basic $auth_b64" \
  --header "X-Apple-I-MD: $oneTimePassword" \
  --header "X-Apple-I-MD-RINFO: $routingInfo" \
  --header "X-Apple-I-MD-M: $machineID" \
  --header "X-Apple-I-TimeZone: $timeZone" \
- --header "X-Apple-I-ClientTime: $clientTime" \
+ --header "X-Apple-I-Client-Time: $clientTime" \
  --header "Content-Type: application/json" \
  --header "Accept: application/json" \
  --header "X-BA-CLIENT-TIMESTAMP: $unixEpoch" \
