09e6a34588298560d5b72e89334237a842757d1f
Chapoly1305 <25316415+Chapoly1305@users.noreply.github.com>
2023-12-05 12:47:31 +0800

    Refactor. Adding new API.

--- web_service.py
+++ web_service.py
@@ -1,10 +1,13 @@
 import datetime
 import json
 import os
+import re
+
 from fastapi import FastAPI
 
 import requests
 from fastapi.params import Query
+from fastapi.responses import JSONResponse
 from pypush_gsa_icloud import icloud_login_mobileme, generate_anisette_headers
 
 app = FastAPI()
@@ -25,14 +28,14 @@ dsid = j['dsid']
 searchPartyToken = j['searchPartyToken']
 
 
-@app.get("/get_encrypted_reports/")
+@app.get("/SingleDeviceEncryptedReports/")
 async def handle_form(
         advertisement_key: str = Query(
             description="Paste your Hashed Advertisement Base64 Key here. Please be aware this is NOT the Private Key, "
-                        "or Public Key! The Hashed Advertisement Key is a Base64 encoded string, that used SHA256 to "
-                        "hash the Advertisement/Public Key.",
-                                       min_length=44, max_length=44, regex=r"^[-A-Za-z0-9+/]*={0,3}$"),
-        hours: int = Query(1, description="Hours to search back in time", ge=1, le=24), ) -> str:
+                        "nor Public Key! The Hashed Advertisement Key is a Base64 encoded string, that used SHA256 to "
+                        "hash the Advertisement/Public Key.This API process one device at a time.",
+            min_length=44, max_length=44, regex=r"^[-A-Za-z0-9+/]*={0,3}$"),
+        hours: int = Query(1, description="Hours to search back in time", ge=1, le=24)):
     unix_epoch = int(datetime.datetime.now().strftime('%s'))
     start_date = unix_epoch - (60 * 60 * hours)
     data = {"search": [{"startDate": start_date * 1000, "endDate": unix_epoch * 1000, "ids": [advertisement_key]}]}
@@ -41,6 +44,40 @@ async def handle_form(
                       auth=(dsid, searchPartyToken),
                       headers=generate_anisette_headers(),
                       json=data)
-    res = r.content.decode()
 
-    return res
+    return json.loads(r.content.decode(encoding='utf-8'))
+
+
+@app.get("/MutipleDeviceEncryptedReports/")
+async def handle_form(
+        advertisement_keys: str = Query(
+            description="Paste your Hashed Advertisement Base64 Key here. Please be aware this is NOT the Private Key, "
+                        "nor Public Key! The Hashed Advertisement Key is a Base64 encoded string, that used SHA256 to "
+                        "hash the Advertisement/Public Key. Sperate each key by a comma.",
+        ),
+        hours: int = Query(1, description="Hours to search back in time", ge=1, le=24)):
+    re_exp = r"^[-A-Za-z0-9+/]*={0,3}$"
+    advertisement_keys_list = []
+    advertisement_keys_invalid_list = []
+    for key in advertisement_keys.strip().split(','):
+        if len(key) != 44 or not re.match(re_exp, key):
+            advertisement_keys_invalid_list.append(key)
+        else:
+            if len(key)>0:
+                advertisement_keys_list.append(key)
+
+    if len(advertisement_keys_invalid_list) > 0:
+        return JSONResponse(
+            content={"error": f"Invalid Hashed Advertisement Base64 Key(s): {advertisement_keys_invalid_list}"},
+            status_code=400)
+
+    unix_epoch = int(datetime.datetime.now().strftime('%s'))
+    start_date = unix_epoch - (60 * 60 * hours)
+    data = {"search": [{"startDate": start_date * 1000, "endDate": unix_epoch * 1000, "ids": advertisement_keys_list}]}
+
+    r = requests.post("https://gateway.icloud.com/acsnservice/fetch",
+                      auth=(dsid, searchPartyToken),
+                      headers=generate_anisette_headers(),
+                      json=data)
+
+    return json.loads(r.content.decode(encoding='utf-8'))
