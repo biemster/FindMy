1d536c9f4cc0ce204f21701e6714c6321a484271
Lars Beemster <biemster@users.noreply.github.com>
2024-10-11 10:21:46 +0200

    update ch592 prep_fw.py with advertisement interval

--- WCH_ch592/README.md
+++ WCH_ch592/README.md
@@ -5,5 +5,8 @@ ch592 FindMy firmware
 The `.bin` can be flashed to the CH592 using [chprog](https://github.com/wagiminator/MCU-Flash-Tools). (Enter the CH592 bootloader by holding down BOOT when connecting it using USB).
 
 ## Preparation
-1. Replace the dummy key in the firmware using the `prep_fw.py` script like this: `./prep_fw.py <path to .keys file>`
+1. Replace the dummy key in the firmware using the `prep_fw.py` script like this (adv-interval is optional, the broadcasting interval in seconds [allowed values are 2,3,5,10,20,30]):
+```bash
+./prep_fw.py --keyfile=<path to .keys file> --adv-interval=5
+```
 2. Flash the new `FindMy_XXXXXXX.bin` file using `chprog FindMy_XXXXXXX`
--- WCH_ch592/prep_fw.py
+++ WCH_ch592/prep_fw.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
-import sys, os
+import os
+import argparse
 from base64 import b64decode
 
-in_keyfile = sys.argv[1]
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--keyfile', help='Path to .keys file', type=str, required=True)
+    parser.add_argument('--adv-interval', help='Advertisement interval (seconds)', type=int, choices=[2,3,5,10,20,30], default=3)
+    args = parser.parse_args()
+    patch_fw(args.keyfile, args.adv_interval)
 
-hashed_adv = ''
-with open(in_keyfile) as keyfile:
-    for line in keyfile:
-        key = line.rstrip('\n').split(': ')
-        if key[0] == 'Advertisement key': hashed_adv = key[1]
-adv = b64decode(hashed_adv)
+def read_key(keyfile):
+    hashed_adv = ''
+    with open(keyfile) as keyfile:
+        for line in keyfile:
+            key = line.rstrip('\n').split(': ')
+            if key[0] == 'Advertisement key': hashed_adv = key[1]
+    return hashed_adv
 
-fw = bytearray(open('main.elf', 'rb').read())
-key_start = fw.find(bytes([0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xef,0xfe,0xdd,0xcc,0xbb,0xaa,0x99,0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11]))
-fw[key_start:key_start +28] = adv
-with open('FindMy_' + os.path.basename(in_keyfile)[:-5] + '.bin', 'wb') as fw_out:
-    fw_out.write(fw)
+def patch_fw(keyfile, adv_interval=3):
+    interval_patches = {2:[0x05,0xc8], 3:[0x05,0x2c], 5:[0x09,0xf4], 10:[0x11,0xe8], 20:[0x21,0xd0], 30:[0x31,0xb8]}
+    adv = b64decode(read_key(keyfile))
+    fw = bytearray(open('main.bin', 'rb').read())
+    key_start = fw.find(bytes([0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xef,0xfe,0xdd,0xcc,0xbb,0xaa,0x99,0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11]))
+    fw[key_start:key_start +28] = adv
+    interval_start = fw.find(bytes([0x05,0x64,0x93,0x05,0x04,0x2c]))
+    fw[interval_start] = interval_patches[adv_interval][0]
+    fw[interval_start +5] = fw[interval_start +15] = interval_patches[adv_interval][1]
+    with open('FindMy_' + os.path.basename(keyfile)[:-5] + '.bin', 'wb') as fw_out:
+        fw_out.write(fw)
+
+if __name__ == '__main__':
+    main()
